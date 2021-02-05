3b6bdb0cc542300040ed4ca2626a7e04a85b68a9
Michael Bartee <michaelbartee@gmail.com>
2021-02-05 13:57:03 -0500

    Initial Commit

---
+++ HWS-GuessTheFlag/HWS_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_GuessTheFlagApp.swift
+//  HWS-GuessTheFlag
+//
+//  Created by Michael Bartee on 2/5/21.
+//
+
+import SwiftUI
+
+@main
+struct HWS_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
